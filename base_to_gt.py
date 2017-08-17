eb3f5f1c4d796fc4369162c32fff5a4a0d110fdf
Steffi <steffi_grote@eva.mpg.de>
2017-08-17 16:24:24 +0200

    add base_to_gt.py

---
+++ base_to_gt.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python3
+
+'''
+given a vcf-file that has bases in last columns instead of genotype,
+convert bases to fake genotype, modify ALT if needed, 
+including adding a third state
+also add header header names of base-individuals from sys.argv[1] (all in one string, ws-separated)
+
+(use case: Kay's script was used to add ape-bases to vcf)
+# CAUTION: this also removes extra char that is added to header by Kays script (visible with 'less file')
+
+'''
+
+
+### helper
+def check_alts(alt, base):
+	''' check if (potentially multiple) ALT match '''
+	alt_alls = alt.split(",")
+	for i, b in enumerate(alt_alls):
+		if b == base: 
+			gt = "/".join(2*str(i+1))
+			return alt, gt
+	alt = alt + "," + base
+	gt = "/".join(2*str(len(alt_alls)+1))
+	return alt, gt
+
+''' test
+check_alts("A","A") == ('A', '1/1')
+check_alts("A","G") == ('A,G', '2/2')
+check_alts("A,G","A") == ('A,G', '1/1')
+check_alts("A,G","G") == ('A,G', '2/2')
+check_alts("A,G","C") == ('A,G,C', '3/3')
+check_alts("A,G,C","T") == ('A,G,C,T', '4/4')
+
+'''
+
+
+def convert_base(ref, alt, base):
+	''' convert one base to GT and update ALT '''
+	base = base.upper()
+	if base == "N" or base == ".":
+		gt = "./."			
+	elif base == ref:
+		gt = "0/0"
+	elif alt == ".":
+		gt = "1/1"
+		alt = base
+	else:
+		alt, gt = check_alts(alt, base)
+	return alt, gt
+
+''' test
+convert_base("A", "C", "A") == ('C', '0/0')
+convert_base("A", "C", "C") == ('C', '1/1')
+convert_base("A", ".", "G") == ('G', '1/1')
+convert_base("A", "C", "G") == ('C,G', '2/2')
+convert_base("A", "C", "N") == ('C', './.')
+convert_base("A", "C", ".") == ('C', './.')
+convert_base("A", "C,G", "T") == ('C,G,T', '3/3')
+
+'''
+
+
+def convert_bases(ref, alt, bases):
+	''' convert list of bases to list of GT and update ALT for every base '''
+	gts = []
+	for base in bases:
+		alt, gt = convert_base(ref, alt, base) # check if this updates alt
+		gts.append(gt)
+	return alt, gts
+
+''' test
+convert_bases("A", "C", ["A","A"]) == ('C', ['0/0', '0/0'])
+convert_bases("A", "C", ["C","N","A"]) == ('C', ['1/1', './.', '0/0'])
+convert_bases("A", ".", [".","G"]) == ('G', ['./.', '1/1'])
+convert_bases("A", "C", ["G","A","T","C","G"]) == ('C,G,T', ['2/2', '0/0', '3/3', '1/1', '2/2'])
+convert_bases("A", "C,G", ["T","C",".","A","T"]) == ('C,G,T', ['3/3', '1/1', './.', '0/0', '3/3'])
+
+'''
+
+###########################################################
+
+
+## TODO: use this if __name__ == __main__ construct
+
+import sys
+
+# variable input
+infile = sys.stdin	 		# input vcf from stream
+new_names = sys.argv[1]		# string with all the names from base individuals	
+
+
+# how many columns with bases? convert new names to tab-separated string
+new_names = new_names.split()
+n = len(new_names)
+new_names = "\t".join(new_names)
+
+# header
+vcf = infile.readline()
+while vcf[:6] != "#CHROM":  
+	sys.stdout.write(vcf[:(-2)*n]+"\n") # remove strange last character that addMAf adds to header
+	vcf = infile.readline()
+last_header = vcf[:(-2)*n] + new_names + "\n"
+sys.stdout.write(last_header)
+
+# body
+for line in infile:
+	line = line.strip().split()
+	bases = line[-n:]
+	ref = line[3]
+	alt = line[4]
+	# go through new bases, adjust ALT and convert to genotype
+	new_alt, new_gts = convert_bases(ref, alt, bases)
+	out = line[:4] + [new_alt] + line[5:-n] + new_gts
+	sys.stdout.write("\t".join(out) + "\n")
+	#sys.stdout.write("\t".join(line) + "\n")
+
+
+
+	
+
+	
