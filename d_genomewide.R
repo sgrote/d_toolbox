8fb18374ecf4971186bb3a7f831f93e8b0dca37f
Steffi <steffi_grote@eva.mpg.de>
2017-11-14 15:56:33 +0100

    handle folders without sites-files

--- d_genomewide.R
+++ d_genomewide.R
@@ -19,8 +19,12 @@ for (i in 1:length(chroms)){
 	setwd(chroms[i])
 	print(getwd())
 	## TODO: only use sites file if one is present
-	sites = as.integer(strsplit(system("wc -l sites", intern=T), " ")[[1]][1])	
-	out_blocks = read.table("out_blocks", header=T)
+    sitesfile = FALSE
+    if ("sites" %in% dir()){
+        sitesfile = TRUE
+        sites = as.integer(strsplit(system("wc -l sites", intern=T), " ")[[1]][1])
+    }    
+    out_blocks = read.table("out_blocks", header=T)
 	pw_sites = read.table("sites_comp", as.is=T, header=T)
 	# jackknife - by-function also returned NULL for unused factor combinations 
 	d = ddply(out_blocks, .(pop1,pop2,pop3,pop4), d_jackknife)
@@ -30,25 +34,29 @@ for (i in 1:length(chroms)){
 	# collect for genomewide
 	if (chroms[i] != "chrX"){
 		if (i==1){
+            if (sitesfile){
+                n_sites_genome = data.frame("chr"=i, "sites"=sites)
+			}
 			genome_pw_sites = pw_sites
-			n_sites_genome = data.frame("chr"=i, "sites"=sites)
-			last_block = 0
+            last_block = 0
 		} else {
+            if (sitesfile){
+                n_sites_genome = rbind(n_sites_genome, c(i, sites))
+            }
 			genome_pw_sites$n_sites = genome_pw_sites$n_sites + pw_sites$n_sites
 			last_block = genome_blocks[nrow(genome_blocks),"block"]
 		}
 		out_blocks$block = out_blocks$block + last_block	
 		genome_blocks = rbind(genome_blocks, out_blocks)
-		if (i!=1) {
-			n_sites_genome = rbind(n_sites_genome, c(i, sites))
-		}
 	} else {
 		message("skipping chrX for genomewide results")
 	}	
 	setwd("../")
 }
 # write genome-wide sites-files
-write.table(n_sites_genome, "n_sites_genome", quote=FALSE, row.names=FALSE)
+if (sitesfile){
+    write.table(n_sites_genome, "n_sites_genome", quote=FALSE, row.names=FALSE)
+}
 # genomewide D
 genome_d = ddply(genome_blocks, .(pop1,pop2,pop3,pop4), d_jackknife) 
 out = merge(genome_d, genome_pw_sites)
