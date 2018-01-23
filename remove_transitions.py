09c48554b7e97a0707e1b5225721c6cc5168c6a7
Steffi <steffi_grote@eva.mpg.de>
2018-01-23 18:06:47 +0100

    remove_transitions.py

---
+++ remove_transitions.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+'''
+restrict to biallelic transversions given index of REF and ALT column (can be applied to several file-formats)
+take file from stdin
+write to stdout
+'''
+
+import sys
+import argparse
+
+
+def main():
+	parser = argparse.ArgumentParser(description='restrict to biallelic transversions given index of REF and ALT column (can be applied to several file-formats). Reads from stdin and writes to stdout.', usage='zcat chr21.vcf.gz | remove_transitions.py > chr21_transversions_only.vcf')
+	# mandatory
+	parser.add_argument("-r","--ref_index", type=int, default=3, help="Column number of REF-allele, 0-based.")
+	parser.add_argument("-a","--alt_index", type=int, default=4, help="Column number of ALT-allele, 0-based.")
+
+	args = parser.parse_args()
+
+	# infile from stdin
+	if sys.stdin.isatty():
+		sys.exit("Error: Needs file from stdin, usage: 'zcat chr21.vcf.gz | remove_transitions.py > chr21_transversions_only.vcf'")
+	infile = sys.stdin
+
+	for line in infile:
+		try:
+			fields = line.rstrip().split()
+			# bases
+			ref = fields[args.ref_index]
+			alt = fields[args.alt_index]
+			# reduce to biallelic 
+			if alt not in "ACTG":
+				continue
+			# reduce to transversions 
+			if ("A" in ref+alt and "G" in ref+alt) or ("C" in ref+alt and "T" in ref+alt):
+				continue
+			# print valid line
+			sys.stdout.write(line)
+		except (IndexError, ValueError) as errore:
+			print(errore)
+			print(line)
+
+if __name__ == "__main__":
+	main()
+
+
+
+
+
