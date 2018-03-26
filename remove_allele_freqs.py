e2407b210ac2ebe13f2da9d08d094df1b3f5f117
Steffi <steffi_grote@eva.mpg.de>
2018-03-26 11:47:54 +0200

    clarify that index is 0-based

--- remove_allele_freqs.py
+++ remove_allele_freqs.py
@@ -14,7 +14,7 @@ import argparse
 def main():
 	parser = argparse.ArgumentParser(description='restrict to sites in range of allele-frequencies given index of allele-freq column (can be applied to several file-formats). Reads from stdin and writes to stdout.', usage='cat freq_file | remove_allele_freqs.py -i 3 --min 0.3 --max 0.7 > freq_file_filtered')
 	# mandatory
-	parser.add_argument("-i","--freq_index", type=int, default=5, help="Column number of allele freqs.")
+	parser.add_argument("-i","--freq_index", type=int, default=5, help="Column number of allele freqs (0-based).")
 	parser.add_argument("--min", type=float, default=0.3, help="Minimum allele freq (inclusive).")
 	parser.add_argument("--max", type=float, default=0.7, help="Maximum allele freq (inclusive).")
 
