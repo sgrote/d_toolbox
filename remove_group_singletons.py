81c98ea0943fb3ce5da6b5a67c431eb8db7d446f
Steffi <steffi_grote@eva.mpg.de>
2019-01-04 14:58:59 +0100

    fix removal of invariable-outside-group-sites if not --var

--- remove_group_singletons.py
+++ remove_group_singletons.py
@@ -29,7 +29,6 @@ def main():
 		sys.exit('Error: Needs vcf from stdin')
 	vcf = sys.stdin
 
-
 	# get list of group samples
 	samples = D.get_pops_from_file(args.group_file)
 	
@@ -43,10 +42,10 @@ def main():
 	sample_header = line.rstrip().split()
 	
 	# get col-numbers for every input sample
-	sample_index = D.get_sample_colnumber(sample_header, samples).values()
+	group_index = D.get_sample_colnumber(sample_header, samples).values()
 	
 	# all other colnums with genotypes
-	outside_index = [i for i in range(9, len(sample_header)) if i not in sample_index]
+	outside_index = [i for i in range(9, len(sample_header)) if i not in group_index]
 
 	county_invar = 0
 	county_private = 0
@@ -55,33 +54,33 @@ def main():
 	for line in vcf:
 		line = line.rstrip().split()
 		try:
-			# leave invariable
+			# no Alt
 			if line[4] == ".":
 				if args.var:
 					county_invar += 1
 				else:
 					sys.stdout.write("\t".join(line) + "\n")
 			else:
-				# for variable sites make sure they are variable outside the group
-				outside_alleles = D.unique_variants(line, outside_index) # e.g [0], [0,2] 
-				if len(outside_alleles) < 2:
-					# there is at most 1 variant outside the group - invariable
-					county_private += 1
+				# invariable outside the group and --var? -> remove
+				outside_alleles = D.unique_variants(line, outside_index) # e.g [0], [0,2]
+				if args.var and len(outside_alleles) < 2:
+					# ALT allele should come from group (despite cases of all-ALT, i.e. county_invar - egal)
+					county_private += 1 
 					continue
-				else:
-					# check for singletons in group
-					inside_alleles = D.unique_variants(line, sample_index)
-					group_single = [i for i in inside_alleles if i not in outside_alleles] 
-					if len(group_single) > 0:
-						# mask
-						for s in sample_index:
-							for g in group_single:
-								line[s] = line[s].replace(g,".")
-						# update ALT
-						line = M.check_complete_alt(line)
-						county_masked += 1
-					sys.stdout.write("\t".join(line) + "\n")
-
+				
+				# variable outside group or not --var?  --> check for singletons and mask
+				inside_alleles = D.unique_variants(line, group_index)
+				group_single = [i for i in inside_alleles if i not in outside_alleles]
+				if len(group_single) > 0:
+					# mask
+					for s in group_index:
+						for g in group_single:
+							line[s] = line[s].replace(g,".")
+					# update ALT
+					line = M.check_complete_alt(line)
+					county_masked += 1
+				sys.stdout.write("\t".join(line) + "\n")
+				
 		except (IndexError, ValueError) as errore:
 			sys.stderr.write(str(errore) + "\n")
 			sys.stderr.write("\t".join(line[:12]) + "\n")
